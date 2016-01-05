2ece9e50cd18aef19204f2f2d1ee4aa59bedd523
PFei-He <498130877@qq.com>
2016-01-05 09:22:49 +0800

    Initial Commit

---
+++ Project/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Project
+//
+//  Created by PFei_He on 16/1/5.
+//  Copyright © 2016年 PF-Lib. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
